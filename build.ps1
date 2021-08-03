c8587c2a4c365020c4363a6104f4031a9119ac10
jyonke <jon.yonke@gmail.com>
2021-08-02 23:37:47 -0400

    Updated Build Script

--- build.ps1
+++ build.ps1
@@ -1,9 +1,27 @@
-$LocalRepository = "$PSScriptRoot\builds"
+#$LocalRepository = "$PSScriptRoot\builds"
+#Register-PSRepository -Name Local_Nuget_Feed -SourceLocation $LocalRepository -PublishLocation $LocalRepository -InstallationPolicy Trusted
+#Publish-Module -Path "$PSScriptRoot\src\" -Repository Local_Nuget_Feed -NuGetApiKey 'ABC123'
+#Unregister-PSRepository -Name Local_Nuget_Feed
 
-New-Item -ItemType Directory -Path $LocalRepository -Force -ErrorAction SilentlyContinue
+#Variables
+$NuspecFile = (Get-ChildItem -Path $PSScriptRoot -Recurse -Filter *.nuspec).FullName
+$BuildDirectory = "$PSScriptRoot\builds"
 
-Register-PSRepository -Name Local_Nuget_Feed -SourceLocation $LocalRepository -PublishLocation $LocalRepository -InstallationPolicy Trusted
+Write-Host "    NUSPEC FILE: $NuspecFile" -ForegroundColor Cyan
+Write-Host "BUILD DIRECTORY: $BuildDirectory" -ForegroundColor Cyan
 
-Publish-Module -Path "$PSScriptRoot\cChocoEx" -Repository Local_Nuget_Feed -NuGetApiKey 'ABC123'
+#Dependencies
+New-Item -ItemType Directory -Path $BuildDirectory -Force -ErrorAction SilentlyContinue | Out-Null
+if (-not(Test-Path "$PSScriptRoot\nuget.exe")) {
+    Invoke-WebRequest -Uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile "$PSScriptRoot\nuget.exe"
+}
 
-Unregister-PSRepository -Name Local_Nuget_Feed
+#Nuget Argumenbts
+$ArgumentList = @(
+    'pack'
+    $NuspecFile
+    "-OutputDirectory $BuildDirectory"
+)
+
+#RUN
+Start-Process -FilePath "$PSScriptRoot\nuget.exe" -ArgumentList $ArgumentList -NoNewWindow -Wait
