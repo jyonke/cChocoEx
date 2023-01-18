2cc03e9f1d23fe5d90a5c4842867a7028b18f632
Jon Yonke <jon.yonke@gmail.com>
2023-01-17 22:41:30 -0500

    fix non admin error

--- src/Private/Test-IsWinOS.OOBE.ps1
+++ src/Private/Test-IsWinOS.OOBE.ps1
@@ -2,9 +2,14 @@ function Test-IsWinOS.OOBE {
     param (
     
     )
-    $ExplorerProcesses = Get-Process explorer -IncludeUserName -ErrorAction SilentlyContinue
+    try {
+        $ExplorerProcesses = Get-Process explorer -IncludeUserName -ErrorAction SilentlyContinue 
+    }
+    catch {
+        Write-Verbose $_.Exception.Message
+    }
     
-    if (    $ExplorerProcesses | Where-Object { ($_.UserName -split '\\' | Select-Object -Last 1) -eq 'defaultuser0' }) {
+    if ($ExplorerProcesses | Where-Object { ($_.UserName -split '\\' | Select-Object -Last 1) -eq 'defaultuser0' }) {
         return $true    
     }
     else {
